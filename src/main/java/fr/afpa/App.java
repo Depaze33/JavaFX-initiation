ae6208a6af04db27782b577052f639bdfbdd623a
Clarisse Otmani <clarisseotm@gmail.com>
2024-07-11 16:34:17 +0200

    Amelioration du programme

--- src/main/java/fr/afpa/App.java
+++ src/main/java/fr/afpa/App.java
@@ -3,13 +3,18 @@ package fr.afpa;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -25,56 +30,64 @@ public class App extends Application {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
 
-        Label form= new Label("Form ");
-        Label enterUser = new Label("Enter user");
-        Label copyEnterUser  = new Label ("Copy of entry");
-        Button copy = new Button ("Recopier");
-        Button erase = new Button ("To erase");
-        Button exitButton = new Button("Exit");
+        Label labelForm = new Label("Form ");
+        Label labelInput = new Label("Enter user");
+        Label labelCopy = new Label("Copy of entry");
+        Button buttonCopy = new Button("Recopier");
+        Button buttonClear = new Button("To erase");
+        Button buttonExit = new Button("Exit");
         TextField textInput = new TextField();
         TextField textOutpout = new TextField();
 
-       GridPane gridPane = new GridPane();
-       gridPane.add(form, 2, 0);
-       gridPane.add (enterUser, 0,1);
-       gridPane.add(copyEnterUser, 0,3);
-       gridPane.add(copy, 4,1);
-       gridPane.add(erase, 4, 2);
-       gridPane.add(exitButton, 4,3);
-       gridPane.add(textInput, 1, 1);
-       gridPane.add(textOutpout, 1, 3);
-     
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        // gridPane.add(labelForm, 2, 0);
+        gridPane.add(labelInput, 0, 0, 2, 1);
+        gridPane.add(textInput, 1, 0);
+        gridPane.add(buttonCopy, 2, 0);
+        gridPane.add(buttonClear, 2, 1);
+        gridPane.add(labelCopy, 0, 2);
+        gridPane.add(buttonExit, 2, 2);
+        gridPane.add(textOutpout, 1, 2);
 
+        VBox vbox = new VBox(labelForm, gridPane);
+        VBox buttonBox = new VBox(10);
+        buttonBox.setAlignment(Pos.TOP_RIGHT);
+        buttonBox.getChildren().addAll(buttonCopy, buttonClear, buttonExit);
 
+        // Ajout du VBox au GridPane
+        gridPane.add(buttonBox, 2, 0, 1, 3);
+        GridPane.setHalignment(buttonBox, HPos.RIGHT);
 
-
-       
-       exitButton.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            System.out.println("Error");
-            stage.close(); // Ferme la fenêtre
-        }
-    });
-    copy.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            textOutpout.setText(textInput.getText());
-        }
-    });
-    erase.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            textInput.clear();
-            textOutpout.clear();
-        }
-    });
 
         
         
-        VBox vbox = new VBox(gridPane);
+
+        buttonExit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Error");
+                stage.close(); // Ferme la fenêtre
+            }
+        });
+        buttonCopy.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textOutpout.setText(textInput.getText());
+            }
+        });
+        buttonClear.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textInput.clear();
+                textOutpout.clear();
+            }
+        });
 
         // vbox.getChildren().addAll(label, exitButton);
+        
+        // VBox vboxDisplay = new VBox(vbox);
 
         Scene scene = new Scene(vbox, 500, 500);
         stage.setScene(scene);
